61e0078b612fcf1670839a0b0a5ed4b6f77e4cc9
Mnaukal <michal.topfer@gmail.com>
2016-03-06 09:42:56 +0100

    Exception checking for new players (don't have score yet)

--- helloWorld.js
+++ helloWorld.js
@@ -3,7 +3,7 @@ handlers.helloWorld = function (args)
     var userInfo = server.GetUserAccountInfo({
         PlayFabId: currentPlayerId
     });
-    
+
     var message = "Args: " + args.name + ", ID: " + currentPlayerId + ", User: " + userInfo.UserInfo.Username;
     log.info(message);
     return { messageValue: message };
@@ -12,29 +12,35 @@ handlers.helloWorld = function (args)
 // update args.score if it's better 
 handlers.postScore = function (args)
 {
-    var currentScore = server.GetPlayerStatistics({
+    var currentScore = -1000000;
+
+    var currentStats = server.GetPlayerStatistics({
         PlayFabId: currentPlayerId,
         StatisticNames: [
             "Score"
         ]
-    }).Statistics[0].Value;
-    
+    });
+
+    if(currentStats.Statistics.length > 0)
+        currentScore = currentStats.Statistics[0].Value;
+
     var updated = false;
     var newScore = currentScore;
+    
     if(args.score > currentScore)
-        {
-            server.UpdatePlayerStatistics({
-                PlayFabId: currentPlayerId,
-                Statistics: [ 
-                    {
-                        StatisticName: "Score",
-                        Value: args.score
-                    }
-                ]
-            });
-            updated = true;
-            newScore = args.score;
-        }
+    {
+        server.UpdatePlayerStatistics({
+            PlayFabId: currentPlayerId,
+            Statistics: [ 
+                {
+                    StatisticName: "Score",
+                    Value: args.score
+                }
+            ]
+        });
+        updated = true;
+        newScore = args.score;
+    }
     return { 
         newScore: newScore, 
         updated: updated
